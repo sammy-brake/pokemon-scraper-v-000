41428a11fe013ced12621cb1b359d8432a9222ae
Samantha brake <sammy.brake@gmail.com>
2018-11-07 04:16:10 +0000

    automatically backed up by learn

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -41,13 +41,13 @@ describe "Pokemon" do
   end
 
   # describe "BONUS" do
-  # 
+  #
   #   before do
   #     @sql_runner.execute_create_hp_column
   #     Pokemon.save('Pikachu', 'electric', @db)
   #     Pokemon.save('Magikarp', 'water', @db)
   #   end
-  # 
+  #
   #   let(:pikachu){Pokemon.find(1, @db)}
   #   let(:magikarp){Pokemon.find(2, @db)}
 
